8435ed6c35e583b0f85ba33135e4fa1ad56a20a8
Monvall <105135454+Monvall@users.noreply.github.com>
2022-12-03 20:16:18 -0600

    referencias

---
+++ Controllers/ReferenciasController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VitaeSystem.Models.ViewModels;
+using VitaeSystem.Models;
+using System.Diagnostics;
+#pragma warning disable 
+
+namespace VitaeSystem.Controllers
+{
+    public class ReferenciasController : Controller
+    {
+        private readonly VitaeSystemContext _referencias;
+        public ReferenciasController(VitaeSystemContext referencias)
+        {
+            _referencias = referencias;
+        }
+        public IActionResult Index(int IDsolicitante)
+        {
+            ListasSolicitantes referencias = new ListasSolicitantes()
+            {
+                listaReferencias = _referencias.Referencias.Where(x => x.Idsolicitante == IDsolicitante)
+                                                     .Include(x => x.OBsolicitante)
+                                                     .ToList()
+            };
+
+            return View(referencias);
+        }
+        [HttpGet]
+        public IActionResult Referencias_Detalle(int Ireferencia)
+        {
+
+            ReferenciaE oReferenciaE = new ReferenciaE()
+            {
+
+                OBreferencia = new Referencia(),
+                listaSolicitante = _referencias.InfoSolicitantes.Select(solicitante => new SelectListItem()
+                {
+
+                    Text = solicitante.Nombres,
+                    Value = solicitante.Idsolicitante.ToString()
+
+                }).ToList()
+
+            };
+
+            if (Ireferencia != 0)
+            {
+                oReferenciaE.OBreferencia = _referencias.Referencias.Find(Ireferencia);
+            }
+
+            return View(oReferenciaE);
+        }
+        [HttpPost]
+        public IActionResult Referencias_Detalle(ReferenciaE objetoU, int Idsolicitante)
+        {
+            if (objetoU.OBreferencia.Idreferencias == 0)
+            {
+                _referencias.Referencias.Add(objetoU.OBreferencia);
+            }
+            else
+            {
+                _referencias.Referencias.Update(objetoU.OBreferencia);
+            }
+
+            _referencias.SaveChanges();
+
+            if (objetoU.OBreferencia.Idreferencias == 0)
+            {
+                return RedirectToAction("Index", "Solicitante");
+            }
+            else
+            {
+                return RedirectToAction("Index", new RouteValueDictionary(new { Controller = "Referencias", Action = "Index", IDsolicitante = Idsolicitante }));
+            }
+
+        }
+    }
+}
